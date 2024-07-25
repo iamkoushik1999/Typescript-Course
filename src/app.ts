71daa247941f26d4d349fb0fe87daf3decd7b19a
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-25 21:42:54 +0530

    Database added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
 import compression from 'compression';
+// DATABASE
+import connectDB from './config/database';
+connectDB();
 
 // APP
 const app = express();
