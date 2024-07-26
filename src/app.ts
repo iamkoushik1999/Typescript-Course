68335651d048c1ff348bccffeef56c7a99505b41
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-26 20:44:00 +0530

    middleware added

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import compression from 'compression';
 // DATABASE
 import connectDB from './config/database';
 connectDB();
+// Error Middleware
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 
 // APP
 const app = express();
@@ -12,5 +14,9 @@ const app = express();
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
