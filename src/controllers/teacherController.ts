33dbf853d6f2bbf39dd414503319530cbfe7cf84
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-30 21:01:40 +0530

    Add: Get Teacher

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -4,7 +4,7 @@ import teacherModel from '../models/teacherModel';
 // ------------------------------------------------------
 
 // POST
-// Add Hospitals
+// Add Teacher
 export const addTeacher = async (req: Request, res: Response) => {
   const {
     fname,
@@ -47,4 +47,13 @@ export const addTeacher = async (req: Request, res: Response) => {
   });
 
   res.status(201).json({ message: 'Teacher Added', data: teacher });
+};
+
+// GET
+// Get Teacher
+export const getTeachers = async (req: Request, res: Response) => {
+  const teachers = await teacherModel.find();
+  const teacherCount = await teacherModel.countDocuments();
+
+  res.status(200).json({ count: teacherCount, data: teachers });
 };
