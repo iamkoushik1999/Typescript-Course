b1c1611efaf320a53a91cfa0ba6cba1d1380fc9e
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-31 21:08:45 +0530

    Add: teacher routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import connectDB from './config/database';
 connectDB();
 // Error Middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware';
+// Import Routes
+import teacherRoutes from './routes/teacherRoutes';
 
 // APP
 const app = express();
@@ -14,6 +16,9 @@ const app = express();
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/v1/teacher', teacherRoutes);
 
 // Error Middleware
 app.use(notFound);
